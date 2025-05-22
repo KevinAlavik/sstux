11ced64bffc412d5c0b59fcd773c9bb717e2c4f3
Kevin Alavik <kevin@alavik.se>
2025-05-22 22:12:03 +0200

    feat: Fixed readme and patched issue regarding something

--- include/config.hpp
+++ include/config.hpp
@@ -4,6 +4,6 @@
 
 namespace SSTux::Config
 {
-    inline constexpr const char *SSTUX_VERSION = "0.1.6";
+    inline constexpr const char *SSTUX_VERSION = "0.1.7";
     inline const std::string WINDOW_TITLE_PREFIX = std::string(" | SSTux v") + SSTUX_VERSION;
 }
